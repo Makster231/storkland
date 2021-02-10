e5ba409232518747be949afeba9740e85f14a9c0
Максим <makster559553@gmail.com>
2021-02-10 16:25:23 +0300

    добавлены модальные окна

--- app/js/component/custom.js
+++ app/js/component/custom.js
@@ -1,6 +1,6 @@
 (function ($, window, document) {
 
-  function sharesChooseSlides() {
+  function ChooseSlides() {
     var mySwiper = new Swiper('.swiper-container', {
         // Optional parameters
         direction: 'horizontal',
@@ -17,6 +17,23 @@
     })
   };
 
+  function modalSlider(){
+    var mySwiper = new Swiper('.swiper-container-modal', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      // Navigation arrows
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      },
+      pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+      },
+  })
+  }
+
   function burgerMenu(){
     $(".icon_burger-toggle").on("click", function(){
       $("body").toggleClass("js_active_burger_menu");
@@ -26,8 +43,13 @@
   $(() => {
     //show content after loaded page
     $("body").css("opacity", "1");
+    $(document).on('afterShow.fb', function( e, instance, slide ) {
+      if(!$(".swiper-container-modal").hasClass("swiper-container-initialized")){
+        modalSlider();
+      }
+    });
     
-    sharesChooseSlides();
+    ChooseSlides();
 
     burgerMenu();
    
--- app/js/scripts.js
+++ app/js/scripts.js
@@ -9,7 +9,7 @@
 })(window.jQuery, window, document);
 
 (function ($, window, document) {
-  function sharesChooseSlides() {
+  function ChooseSlides() {
     var mySwiper = new Swiper('.swiper-container', {
       // Optional parameters
       direction: 'horizontal',
@@ -28,6 +28,23 @@
 
   ;
 
+  function modalSlider() {
+    var mySwiper = new Swiper('.swiper-container-modal', {
+      // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      // Navigation arrows
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+      }
+    });
+  }
+
   function burgerMenu() {
     $(".icon_burger-toggle").on("click", function () {
       $("body").toggleClass("js_active_burger_menu");
@@ -37,7 +54,12 @@
   $(function () {
     //show content after loaded page
     $("body").css("opacity", "1");
-    sharesChooseSlides();
+    $(document).on('afterShow.fb', function (e, instance, slide) {
+      if (!$(".swiper-container-modal").hasClass("swiper-container-initialized")) {
+        modalSlider();
+      }
+    });
+    ChooseSlides();
     burgerMenu();
   });
 })(window.jQuery, window, document);
